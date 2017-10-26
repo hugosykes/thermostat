b142ef6e280196862f7c9c1e3840f5cc49e63e42
hugosykes <hugo@denmeadfarm.co.uk>
2017-10-26 18:34:30 +0100

    Added CSS and lots of jQuery commands!

--- src/jquery_commands.js
+++ src/jquery_commands.js
@@ -1,5 +1,30 @@
 $(document).ready(function() {
+  var thermostat = new Thermostat();
+  $('#temperature').html(thermostat.temperature());
 
-$(".tempDisplay").hide(1000);
+  // $(".tempDisplay").hide(2000);
+  $('#temperature-up').click(function() {
+    thermostat.up();
+    $('#temperature').html(thermostat.temperature());
+    $('#usage').html(thermostat.currentEnergyUsage());
+  });
+
+  $('#temperature-down').click(function() {
+    thermostat.down();
+    $('#temperature').html(thermostat.temperature());
+    $('#usage').html(thermostat.currentEnergyUsage());
+  });
+
+  $('#temperature-reset').click(function() {
+    thermostat.reset();
+    $('#temperature').html(thermostat.temperature());
+    $('#usage').html(thermostat.currentEnergyUsage());
+  });
+
+  $('#powerSave').click(function() {
+    thermostat.powerSavingMode();
+  });
+
+  $('#usage').html(thermostat.currentEnergyUsage());
 
 });
